53a5d9415db04d46d12b0c7f61abfc1ee6a0b512
Oziach <bankheleishan@gmail.com>
2024-08-25 17:32:37 +0530

    Added Pulpit script for the pulpit lifecycle
    I used a state machine for this instead of animator, which was the other option.

---
+++ Doofus Adventure/Assets/Scripts/Pulpit.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//growing and shrinking can also be done using animator quite easily,
+//but doing it procedurally using state machines is cool.
+
+public class Pulpit : MonoBehaviour
+{
+    [SerializeField] private float scaleDuration; //time for pulpit to grow or shrink when created or destroyed
+    private float scaleSpeed;
+
+    [SerializeField] private float pulpitDuration = 5f;
+
+    enum PulpitState {
+        Growing,
+        Countdown,
+        Shrinking
+    }
+
+    [SerializeField] PulpitState currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = PulpitState.Growing;
+        scaleSpeed = 1/scaleDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //grow
+        if (currentState == PulpitState.Growing) {
+            Grow();
+        }
+        //maintain
+        else if (currentState == PulpitState.Countdown) {
+
+            pulpitDuration -= Time.deltaTime;
+
+            if (pulpitDuration <= 0) {
+                currentState = PulpitState.Shrinking;
+            }
+        }
+        //shrink
+        else {
+            Shrink();
+        }
+    }
+
+    public void Grow() {
+        transform.localScale += Vector3.one * scaleSpeed * Time.deltaTime;
+        if (transform.localScale.x > 1) { transform.localScale = Vector3.one; }
+
+        if (transform.localScale == Vector3.one) { currentState = PulpitState.Countdown; }
+    }
+
+    public void Shrink() {
+        transform.localScale -= Vector3.one * scaleSpeed * Time.deltaTime;
+        if (transform.localScale.x < 0) { transform.localScale = Vector3.zero; }
+
+        if (transform.localScale == Vector3.zero) {
+            Destroy(gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+    
+}
